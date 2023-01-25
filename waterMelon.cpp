a749664b18dbc5ce9e7c887151a70968f762ffb5
MD Shahreer Irfan <99862033+ShahreerIrfan@users.noreply.github.com>
2023-01-25 21:23:37 +0600

    Add files via upload

---
+++ waterMelon.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(void){
+    int num;
+    cin>>num;
+    if(num%2==0&&num>2){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+    return 0;
+}
