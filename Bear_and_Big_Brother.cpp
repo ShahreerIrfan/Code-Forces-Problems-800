baa886e7d8b6cb8aa82a93c85abe00b434e8ca6d
MD Shahreer Irfan <99862033+ShahreerIrfan@users.noreply.github.com>
2023-04-07 16:00:35 +0600

    Add files via upload

---
+++ Bear_and_Big_Brother.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(void){
+    int x,y;
+    cin>>x>>y;
+    int year = 0;
+    while(x<=y){
+        x*=3;
+        y*=2;
+        year++;
+    }
+    cout<<year<<endl;
+}
