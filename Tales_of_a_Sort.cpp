00b1deea134f5fed56ed7455d49aeea47335c609
unknown <mdshahreerirfan@gmail.com>
2023-11-16 00:24:26 +0600

    Tales_of_a_Sort

--- Tales_of_a_Sort.cpp
+++ Tales_of_a_Sort.cpp
@@ -9,10 +9,10 @@ int main(){
         for(int i=0;i<n;i++)cin>>arr[i];
 
         int maxi = 0;
-        for(int i=0;i<n;i++){
-            if(arr[i]>arr[i+1]){
-                if(maxi<arr[i]){
-                    maxi = arr[i];
+        for(int i=1;i<n;i++){
+            if(arr[i]<arr[i-1]){
+                if(maxi<arr[i-1]){
+                    maxi = arr[i-1];
                 }
             }
         }
