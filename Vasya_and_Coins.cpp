b56f853c06c1e29d366d06aaceb6ebb9ee9105bb
unknown <mdshahreerirfan@gmail.com>
2023-11-16 14:08:17 +0600

    vasya and coins

--- Vasya_and_Coins.cpp
+++ Vasya_and_Coins.cpp
@@ -1,1 +1,16 @@
-#include
+#include<bits/stdc++.h>
+using namespace std;
+signed main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        if(a==0)cout<<1<<endl;
+        else cout<< b*2 + a + 1<<endl;
+    }
+    return 0;
+}
